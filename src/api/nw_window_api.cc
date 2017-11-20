ce7ad14656d76613dd69d6adfab01a72a6b76825
Jan Rucka <ruckajan10@gmail.com>
2017-11-20 16:34:07 +0100

    Hot-fix of occasional crash when pop-up window was closed.

--- src/api/nw_window_api.cc
+++ src/api/nw_window_api.cc
@@ -147,12 +147,14 @@ bool NwCurrentWindowInternalCloseFunction::RunAsync() {
 
   bool force = params->force.get() ? *params->force : false;
   AppWindow* window = getAppWindow(this);
-  if (force)
-    base::MessageLoop::current()->task_runner()->PostTask(FROM_HERE,
-         base::Bind(&NwCurrentWindowInternalCloseFunction::DoClose, window));
-  else if (window->NWCanClose())
-    base::MessageLoop::current()->task_runner()->PostTask(FROM_HERE,
-         base::Bind(&NwCurrentWindowInternalCloseFunction::DoClose, window));
+  if (window) {
+    if (force)
+      base::MessageLoop::current()->task_runner()->PostTask(FROM_HERE,
+        base::Bind(&NwCurrentWindowInternalCloseFunction::DoClose, window));
+    else if (window->NWCanClose())
+      base::MessageLoop::current()->task_runner()->PostTask(FROM_HERE,
+        base::Bind(&NwCurrentWindowInternalCloseFunction::DoClose, window));
+  }
 
   SendResponse(true);
   return true;
